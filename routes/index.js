91395586eb88ebf93b9db50f7da72a2eb2883e00
Ed de Almeida <edvaldoajunior@gmail.com>
2022-04-25 19:48:45 -0300

    Páginas e menu principal

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/upload', function(req, res, next) {
+  res.render('upload');
+});
+
+router.get('/search', function(req, res, next) {
+  res.render('search');
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about');
 });
 
 module.exports = router;
