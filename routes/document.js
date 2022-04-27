4d22140374fe03d913c51ef36ebc9c7990ac7f44
Ed de Almeida <edvaldoajunior@gmail.com>
2022-04-27 18:29:16 -0300

    Fazendo o upload

--- routes/document.js
+++ routes/document.js
@@ -1,15 +1,32 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
 
+const multer = require('multer')
+const multerConfig = require('../config/multer')
 
-router.post('/upload', function(req, res, next) {
-  let text = "<ul>"
-  Object.keys(req).map(item => {
-    text += "<li>" + item + "</li>";
-    return null;
+const router = express.Router()
+
+router.post('/upload', multer(multerConfig).single('file'), function (
+  req,
+  res,
+  next
+) {
+  const {
+    author_name,
+    author_middle,
+    author_surname,
+    book_title,
+    book_type,
+    book_language,
+    book_file,
+    file
+  } = req.body
+  res.render('upcomplete', {
+    author: author_name + ' ' + author_middle + ' ' + author_surname,
+    title: book_title,
+    type: book_type,
+    language: book_language,
+    file: book_file
   })
-  text += "</ul>"
-  res.send(text);
-});
+})
 
-module.exports = router;
+module.exports = router
